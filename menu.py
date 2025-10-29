5dd7c3c969ee6549f24a1eced03aae2839ce96b3
280liv-blip <280dogslivia@gmail.com>
2025-10-29 16:32:25 -0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,14 @@
+print ("======MENU PRINCIPAL======")
+print("1 para incluir")
+print("2 para alterar")
+
+opcao = input ("sua opcao :")
+
+if opcao == "1" :
+    import incluir 
+elif opcao == "2" :
+    import alterar 
+else :
+    print ("ERRO!seu boco é só 1 ou 2")    
+
+
